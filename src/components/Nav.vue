81ecea0af503af3afe9bd549b491cfff8e0ba0c7
ywg <2521408103@qq.com>
2022-06-23 13:04:25 +0800

    fix

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,15 @@
 <template>
-<div>
-  <ul class="nav">
+  <!-- <div > -->
+  <ul :class="{ nav: !navShow, tableLayout: navShow, }">
     <li v-for="(item, index) in navList" :key="index" @click="handleClickNav(index)"
       :class="{ active: currentNav === index }">
       {{ item }}
     </li>
-    <div class="tbl-list"><img
+    <div :class="{ 'tbl-list': !navShow, 'tbl-list-show': navShow }" @click="handleNavSwitch"><img
         src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAYCAYAAADpnJ2CAAABB0lEQVRIS+2UMUvEQBSE571lSSFcI8IV11lZWGhjJ0S4ImkCafyl2c1ZaOuvEOVAQc/i4CAS73Ykiohomc01tz9gPmZ23oj3/g1AgvhvKSJH4r1nfNYXQUQuO+AcwCQ2lOSdtfZUnHMjkmfGGBMRuk6S5DZN05VEhPwrvQP2nvjwkTrnDlV1CiBaS0muAcyyLLvv7nAFYK/37P4KLkIIx1tZmicA4wEcPojI59IcALggGe0PVfW9aZrrsixfh2/pAFH+Quwc9p5419IJyamqau/qP4Kbtm2viqJ47A5/CWAUEfYt/ayqW1iauq4XJPcHcDi31p5IVVVjY8x5zKUJIWxE5CbP85cPQQ1jDeqW/S8AAAAASUVORK5CYII="
         alt="更多"></div>
   </ul>
-</div>
+  <!-- </div> -->
 </template>
 
 <script>
@@ -33,19 +33,31 @@ export default {
         "行业协会",
         "大型活动",
       ],
-
+      navShow: false
     };
   },
   created() {
     this.currentNav = 0;
   },
   methods: {
+    changeIndex(i) {
+      const _ = this.navList[i];
+      this.navList.splice(i, 1)
+      this.navList.unshift(_)
+    },
     handleClickNav(i) {
       if (this.currentNav !== i) {
         this.currentNav = i;
         this.$store.dispatch("loadData", { id: i - 1, index: 1 });
+
+        this.navShow && this.changeIndex(i)
       }
+      this.navShow = false;
+
     },
+    handleNavSwitch() {
+      this.navShow = true;
+    }
   },
 };
 </script>
@@ -55,6 +67,7 @@ ul.nav {
   list-style-type: none;
   padding: 0;
   width: 118px;
+  max-height: 95%;
   margin: 0 auto;
   background: #fff;
   box-shadow: 0 10px 20px 0 hsl(0deg 0% 67% / 10%);
@@ -66,6 +79,7 @@ ul.nav {
   top: 50%;
   transform: translateY(-50%);
   display: inline-block;
+
 }
 
 li {
@@ -96,31 +110,55 @@ li:hover {
 .tbl-list {
   display: none;
 }
-  ul.nav:nth-child(n + 3) {
-    display: none;
-    visibility: hidden;
-  }
+
+
 @media screen and (max-width: 800px) {
   ul.nav {
+    list-style-type: none;
+    margin: 0 auto;
+    background: #fff;
+    box-shadow: 0 10px 20px 0 hsl(0deg 0% 67% / 10%);
+    /* overflow: hidden; */
+    /* display: inline-block; */
     padding: 3%;
     box-sizing: border-box;
     width: 100%;
     height: 61px;
     line-height: 61px;
+    /* max-height: 60vh; */
     border-radius: 0;
-
+    transform: translateY(0);
     position: fixed;
     left: 0;
     top: 0;
-    transform: translateY(0);
-    display: flex;
+    display: grid;
     justify-content: space-between;
     z-index: 2;
   }
 
-  ul.nav:nth-child(n + 3) {
+  .tableLayout {
+    /* display: table !important; */
+    /* height: unset !important; */
+    overflow: auto;
+    justify-content: center;
+    /* display: flex !important;
+    flex-direction: column; */
+    line-height: 61px;
+    height: 88vh;
+    position: fixed;
+    left: 0;
+    top: 0;
+    transform: translateY(0);
+    z-index: 2;
+    background: rgba(255, 255, 255, 0.9);
+    box-shadow: 0 10px 20px 0 hsl(0deg 0% 67% / 10%);
+    max-height: 88vh;
+    padding: 0;
+    margin: 0;
+  }
+
+  .tbl-list-show {
     display: none;
-    visibility: hidden;
   }
 
   .tbl-list {
